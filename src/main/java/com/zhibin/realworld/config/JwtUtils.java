2186e30929d6af2651a54d9b2cb56771e0496b8f
MokaJun <1296939448@qq.com>
2023-03-01 21:18:41 +0800

    feat: add jwt util part1

---
+++ src/main/java/com/zhibin/realworld/config/JwtUtils.java
@@ -0,0 +1,52 @@
+package com.zhibin.realworld.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.impl.DefaultClaims;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.time.Instant;
+import java.util.Date;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
+
+@Component
+public class JwtUtils {
+    private String jwtSigningKey = "secret";
+
+    public String extractSub(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> function) {
+        Claims claims = extractAllClaims(token);
+        return function.apply(claims);
+    }
+
+    public Claims extractAllClaims(String token) {
+        return Jwts.parser().setSigningKey(jwtSigningKey).parseClaimsJws(token).getBody();
+    }
+
+    public String createToken(Map<String, Object> claims, UserDetails userDetails) {
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(userDetails.getUsername())
+                .claim("authorities", userDetails.getAuthorities())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + TimeUnit.HOURS.toMillis(24)))
+                .signWith(SignatureAlgorithm.HS256, jwtSigningKey)
+                .compact();
+    }
+
+
+}
