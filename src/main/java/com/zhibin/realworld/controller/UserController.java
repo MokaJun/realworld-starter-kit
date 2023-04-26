c36a0bfe8c7f858ff1798669325c0e599a68c064
“Zhibin <zhibin.kang@thoughtworks.com>
2023-04-26 14:48:08 +0800

    refactor: rename controller

--- src/main/java/com/zhibin/realworld/controller/UserController.java
+++ src/main/java/com/zhibin/realworld/controller/UserController.java
@@ -3,18 +3,15 @@ package com.zhibin.realworld.controller;
 
 import com.zhibin.realworld.controller.request.LoginRequest;
 import com.zhibin.realworld.entity.User;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.ServletRequest;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-import java.util.Map;
 
-@RequestMapping("/api/users/login")
+@RequestMapping("/api/users")
 @RestController
-public class LoginController {
-    @PostMapping
+public class UserController {
+    @PostMapping("/login")
     public User login(HttpServletRequest request, @RequestBody LoginRequest loginRequest){
         String username = loginRequest.getUser().getEmail();
         String password = loginRequest.getUser().getPassword();
