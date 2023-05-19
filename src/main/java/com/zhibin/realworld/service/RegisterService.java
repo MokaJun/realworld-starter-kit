3d1ae7b312a064c12e4d06d9ff2e0ba2aafe02c5
“Zhibin <zhibin.kang@thoughtworks.com>
2023-05-19 11:08:45 +0800

    fix

--- src/main/java/com/zhibin/realworld/service/RegisterService.java
+++ src/main/java/com/zhibin/realworld/service/RegisterService.java
@@ -3,7 +3,9 @@ package com.zhibin.realworld.service;
 import com.zhibin.realworld.controller.request.RegisterRequest;
 import com.zhibin.realworld.controller.response.RegisterResponse;
 import com.zhibin.realworld.entity.User;
+import org.springframework.stereotype.Service;
 
+@Service
 public class RegisterService {
 
     public RegisterResponse register(RegisterRequest request) {
