7d1efee86fb84dea051067e476d43fcd36e6680d
“Zhibin <zhibin.kang@thoughtworks.com>
2023-05-23 15:52:33 +0800

    remove unused anno

--- src/main/java/com/zhibin/realworld/controller/request/LoginRequest.java
+++ src/main/java/com/zhibin/realworld/controller/request/LoginRequest.java
@@ -1,17 +1,10 @@
 package com.zhibin.realworld.controller.request;
 
 import com.fasterxml.jackson.annotation.JsonRootName;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-import java.util.Map;
 
-@NoArgsConstructor
-@AllArgsConstructor
 @Getter
-@Setter
 @JsonRootName("user")
 public class LoginRequest {
     private String email;
